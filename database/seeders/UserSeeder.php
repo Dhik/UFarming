824e5122cd0f1a3a7d63d3409d9b9908fb1c13bd
Rashomon <mohammad18018@mail.unpad.ac.id>
2020-11-09 15:01:19 +0700

    seeder_users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+// use Faker\Factory as Faker;
+use DB;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach(range(1,10) as $item)
+    {
+        DB::table('users')->insert([
+            'id' => $item,
+            'name' => "name $item",
+            'email' => "email $item",
+            'profile_picture' => 'dhikri.jpg',
+            'password' => "password $item", 
+            'active_plant' => $item,
+            'location' => "loc $item",
+            'username' => "username $item",
+        ]);
+    }
+    }
+}
