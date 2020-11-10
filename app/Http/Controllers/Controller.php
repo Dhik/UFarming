ff4b6c84d79233b2d845f28fd588dd1b725efa53
Asep Budiyana M <unkn0wn@localhost.localdomain>
2020-11-10 21:45:07 +0700

    set exipired token

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,7 @@ class Controller extends BaseController
         return response()->json([
             'token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => Auth::factory()->getTTL() * 60 * 24 * 30
+            'expires_in' => Auth::factory()->getTTL() * 1
         ], 200);
     }
 }
