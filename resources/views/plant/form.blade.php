1a718dd633fad2736388b06ae36948864e95be24
Ahmad Faaiz Al-auza'i <afaaiz2@gmail.com>
2020-11-18 18:32:11 +0700

    Update UI add plant data

--- resources/views/plant/form.blade.php
+++ resources/views/plant/form.blade.php
@@ -2,63 +2,267 @@
 <html>
 <head>
 	<title>Admin | Add Plant</title>
+
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
 </head>
 <body>
 
-	<h3>Plant Data</h3>
 
-	<a href="/admin/plant"> Back</a>
-	
-	<br/>
-	<br/>
 
-	<form action="/admin/plant/store" method="post" enctype="multipart/form-data">
-		Plant Name <input type="text" name="plant_name" required="required"> <br/>
-		Summary <textarea name="summary" required="required"></textarea> <br/>
-		Growing <textarea name="growing" required="required"></textarea> <br/>
-		Harvesting <textarea name="harvesting" required="required"></textarea> <br/>
+  <div class="container mx-auto py-24 px-12">
+    <form class="w-full" action="/admin/plant/store" method="post" enctype="multipart/form-data">
+
+      <div class="flex items-center justify-between mb-12">
+        <div class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          <a href="/admin/plant"> Back</a>
+        </div>
+      </div>
+      
+      <p class="text-3xl mb-6">Add Plant Data</p>
+
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                Plant Name
+              </label>
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Plant Name" name="plant_name" required="required">
+            </div>
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                Difficulty
+              </label>
+              <div class="relative">
+                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" name="difficulty" required>
+                  <option value="Easy">Easy</option>
+                  <option value="Medium">Medium</option>
+                  <option value="Hard">Hard</option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                Category
+              </label>
+              <div class="relative">
+                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" name="category" required>
+                  @foreach($category as $c)
+                    <option value="{{ $c->id }}">{{ $c->category_name }}</option>
+                  @endforeach
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                Type
+              </label>
+              <div class="relative">
+                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" name="type" required>
+                  @foreach($type as $t)
+                    <option value="{{ $t->id }}">{{ $t->type_name }}</option>
+                  @endforeach
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+              </div>
+            </div>
+
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                Stages
+              </label>
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="number" name="stages" required="required" placeholder="10">
+            </div>
+
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                Total Days
+              </label>
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="number" name="total_days" required="required" placeholder="30">
+            </div>
+          
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                Summary
+              </label>
+              <textarea class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" rows="4" name="summary" required="required"></textarea>
+            </div>
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                Growing
+              </label>
+              <textarea class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" rows="4" name="growing" required="required"></textarea>
+            </div>
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                Harvesting
+              </label>
+              <textarea class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" rows="4" name="harvesting" required="required"></textarea>
+            </div>
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                Picture
+              </label>
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="file" name="picture" required="required">
+            </div>
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                Success Rate
+              </label>
+              <input class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="range" min="1" max="100" value="50" name="success_rate">
+            </div>
+          </div>
+        </div>
+
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0 sm:pl-6 lg:pl-12">
+          <p class="text-xl mb-6">Statistic Data</p>
+
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+            Germ Days
+          </label>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="germ_days_low" required="required" placeholder="Min">
+            </div>
+            
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="germ_days_up" required="required" placeholder="Max">
+            </div>
+          </div>
+
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+            Germ Temperature
+          </label>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="germ_temperature_low" required="required" placeholder="Min">
+            </div>
+            
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="germ_temperature_up" required="required" placeholder="Max">
+            </div>
+          </div>
+
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+            Growth Days
+          </label>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="growth_days_low" required="required" placeholder="Min">
+            </div>
+            
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="growth_days_up" required="required" placeholder="Max">
+            </div>
+          </div>
+
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+            Height
+          </label>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="height_low" required="required" placeholder="Min">
+            </div>
+            
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="height_up" required="required" placeholder="Max">
+            </div>
+          </div>
+
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+            PH
+          </label>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="pH_low" required="required" placeholder="Min">
+            </div>
+            
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="pH_up" required="required" placeholder="Max">
+            </div>
+          </div>
+
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+            Spacing
+          </label>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="spacing_low" required="required" placeholder="Min">
+            </div>
+            
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="spacing_up" required="required" placeholder="Max">
+            </div>
+          </div>
+
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+            Temperature
+          </label>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="temperature_low" required="required" placeholder="Min">
+            </div>
+            
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="temperature_up" required="required" placeholder="Max">
+            </div>
+          </div>
+
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+            Width
+          </label>
+          <div class="flex flex-wrap -mx-3 mb-12">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="width_low" required="required" placeholder="Min">
+            </div>
+            
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" name="width_up" required="required" placeholder="Max">
+            </div>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Save">
+              Save
+            </button>
+          </div>
+
+        </div>
         
-        Picture <input type="file" name="picture" required="required"> <br/>
-        Difficulty
-        <select name="difficulty" required>
-          <option value="Easy">Easy</option>
-          <option value="Medium">Medium</option>
-          <option value="Hard">Hard</option>
-        </select> <br/>
-        
-        Category 
-        <select name="category" required>
-        @foreach($category as $c)
-          <option value="{{ $c->id }}">{{ $c->category_name }}</option>
-        @endforeach
-        </select> <br/>
-        
-        Type 
-        <select name="type" required>
-        @foreach($type as $t)
-          <option value="{{ $t->id }}">{{ $t->type_name }}</option>
-        @endforeach
-        </select> <br/>
+      </div>
 
-        Stages <input type="number" name="stages" required="required"> <br/>
-        Total Days <input type="number" name="total_days" required="required"> <br/>
-        Success Rate <input type="range" min="1" max="100" value="50" name="success_rate">
-    
-    <h3>Statistic Data</h3>
-
-      Germ Days <input type="text" name="germ_days_low" required="required"> - <input type="text" name="germ_days_up" required="required"> <br/>
-      Germ Temperature <input type="text" name="germ_temperature_low" required="required"> - <input type="text" name="germ_temperature_up" required="required"> <br/>
-      Growth Days <input type="text" name="growth_days_low" required="required"> - <input type="text" name="growth_days_up" required="required"> <br/>
-      Height <input type="text" name="height_low" required="required"> - <input type="text" name="height_up" required="required"> <br/>
-      PH <input type="text" name="pH_low" required="required"> - <input type="text" name="pH_up" required="required"> <br/>
-      Spacing <input type="text" name="spacing_low" required="required"> - <input type="text" name="spacing_up" required="required"> <br/>
-      Temperature <input type="text" name="temperature_low" required="required"> - <input type="text" name="temperature_up" required="required"> <br/>
-      Width <input type="text" name="width_low" required="required"> - <input type="text" name="width_up" required="required"> <br/>
-
-    <br/>
-
-    <input type="submit" value="Save"> 
-  </form>
+    </form>
+  </div>
 
 </body>
 </html>
