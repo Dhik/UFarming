ee277fb93eee3b9fe3a982c228edc7a9ddae1ac5
Asep Budiyana M <unkn0wn@localhost.localdomain>
2020-12-01 06:03:21 +0700

    styling admin index

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
 	<title>Admin | Get Article</title>
@@ -30,10 +30,10 @@
             <td><img src="{{ $a->picture }}" width=200></td>
             <td>{{ $a->source }}</td>
             <td>{{ $a->author }}</td>
-			<td>
+			<td> -->
                 <!-- <a href="/admin/plant/detail/{{ $a->id }}">Detail</a>
 				| -->
-				<a href="/admin/article/edit/{{ $a->id }}">Edit</a>
+				<!-- <a href="/admin/article/edit/{{ $a->id }}">Edit</a>
 				|
 				<a href="/admin/article/delete/{{ $a->id }}">Delete</a>
 			</td>
@@ -42,4 +42,74 @@
 	</table>
 
 </body>
-</html>
+</html> -->
+@extends('layouts.app2')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0 text-dark">Article Page</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Article</a></li>
+                <li class="breadcrumb-item active">Main </li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <a href="/admin/article/add" class="btn btn-primary"> + Add New Article</a>
+                        </div>    
+                    <div class="card-body">
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <thead>
+                                <tr>
+									<th>Title</th>
+									<th>Category</th>
+									<th>Description</th>
+            						<th>Picture</th>
+            						<th>Source</th>
+            						<th>Author</th>
+									<th>Option</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($articles as $a)
+                                    <tr>
+										<td>{{ $a->title }}</td>
+										<td>{{ $a->category }}</td>
+										<td>{{ $a->description }}</td>
+            							<td><img src="{{ $a->picture }}" width=200></td>
+            							<td>{{ $a->source }}</td>
+            							<td>{{ $a->author }}</td>
+										<td>
+                							<!-- <a href="/admin/plant/detail/{{ $a->id }}">Detail</a>
+											| -->
+											<a href="/admin/article/edit/{{ $a->id }}" class="btn btn-warning">Edit</a>
+											|
+											<a href="/admin/article/delete/{{ $a->id }}" class="btn btn-danger">Delete</a>
+										</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
--- resources/views/plant/index.blade.php
+++ resources/views/plant/index.blade.php
@@ -1,52 +1,76 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Admin | Get Plant</title>
-</head>
-<body>
+@extends('layouts.app2')
 
-	<h3>Plant Data</h3>
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0 text-dark">Plant Page</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Plant</a></li>
+                <li class="breadcrumb-item active">Main </li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
 
-	<a href="/admin/plant/add"> + Add New Plant</a>
-	
-	<br/>
-	<br/>
-
-	<table border="1">
-		<tr>
-			<th>Plant Name</th>
-			<th>Summary</th>
-			<th>Growing</th>
-			<th>Harvesting</th>
-            <th>Picture</th>
-            <th>Difficulty</th>
-            <th>Stages</th>
-            <th>Total Days</th>
-            <th>Success Rate</th>
-			<th>Option</th>
-		</tr>
-		@foreach($plants as $p)
-		<tr>
-			<td>{{ $p->plant_name }}</td>
-			<td>{{ $p->summary }}</td>
-			<td>{{ $p->growing }}</td>
-			<td>{{ $p->harvesting }}</td>
-            <td><img src="{{ $p->picture }}" width=200></td>
-            <td>{{ $p->difficulty }}</td>
-            <td>{{ $p->stages }}</td>
-            <td>{{ $p->total_days }}</td>
-            <td>{{ $p->success_rate }}</td>
-			<td>
-                <!-- <a href="/admin/plant/detail/{{ $p->id }}">Detail</a>
-				| -->
-				<a href="/admin/plant/edit/{{ $p->id }}">Edit</a>
-				|
-				<a href="/admin/plant/delete/{{ $p->id }}">Delete</a>
-			</td>
-		</tr>
-		@endforeach
-	</table>
-
-
-</body>
-</html>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <a href="/admin/plant/add" class="btn btn-primary"> + Add New Plant</a>
+                        </div>    
+                    <div class="card-body">
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <thead>
+                                <tr>
+									<th>Plant Name</th>
+									<th>Summary</th>
+									<th>Growing</th>
+									<th>Harvesting</th>
+            						<th>Picture</th>
+            						<th>Difficulty</th>
+            						<th>Stages</th>
+            						<th>Total Days</th>
+            						<th>Success Rate</th>
+									<th>Option</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($plants as $p)
+                                    <tr>
+										<td>{{ $p->plant_name }}</td>
+										<td>{{ $p->summary }}</td>
+										<td>{{ $p->growing }}</td>
+										<td>{{ $p->harvesting }}</td>
+            							<td><img src="{{ $p->picture }}" width=200></td>
+            							<td>{{ $p->difficulty }}</td>
+            							<td>{{ $p->stages }}</td>
+            							<td>{{ $p->total_days }}</td>
+            							<td>{{ $p->success_rate }}</td>
+										<td>
+                							<!-- <a href="/admin/plant/detail/{{ $p->id }}">Detail</a>
+											| -->
+											<a href="/admin/plant/edit/{{ $p->id }}" class="btn btn-warning">Edit</a>
+											|
+											<a href="/admin/plant/delete/{{ $p->id }}" class="btn btn-danger">Delete</a>
+										</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
