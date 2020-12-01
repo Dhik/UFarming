07dbcd1e61227b1a954dca0772aa7b4f97d30ffa
Asep Budiyana M <unkn0wn@localhost.localdomain>
2020-12-01 07:07:13 +0700

    styling form admin

--- resources/views/article/edit.blade.php
+++ resources/views/article/edit.blade.php
@@ -2,28 +2,87 @@
 <html>
 <head>
 	<title>Admin | Add Article</title>
+
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
 
-	<h3>Article Data</h3>
+  <div class="container mx-auto py-24 px-12">
 
-	<a href="/admin/article"> Back</a>
-	
-	<br/>
-	<br/>
-    
-    <form action="/admin/article/update" method="post" enctype="multipart/form-data">
-    @foreach($article as $a)
-        <input type="hidden" name="id" value="{{ $a->id }}"> <br/>
-	    Title <input type="text" name="title" required="required" value="{{ $a->title }}"> <br/>
-        Category <input type="text" name="category" required="required" value="General"> <br/>
-	    Description <textarea name="description" required="required">{{ $a->description }}</textarea> <br/>
-        Picture <input type="file" name="picture" required="required"> <br/>
-        Source <input type="text" name="source" required="required" value="{{ $a->source }}"> <br/>
-        Author <input type="text" name="author" required="required" value="{{ $a->author }}"> <br/>  
-    @endforeach  
-    <input type="submit" value="Save"> 
-  </form>
+  @foreach($article as $a)
+    <form class="w-full" action="/admin/article/update" method="post" enctype="multipart/form-data">
+      <input type="hidden" name="id" value="{{ $a->id }}"> <br/>
+
+		  <div class="flex items-center justify-between mb-12">
+    	    <div class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+    	      <a href="/admin/article"> Back</a>
+    	    </div>
+    	</div>
+		
+		<p class="text-3xl mb-6">Edit Article Data</p>
+
+		<div class="flex flex-wrap -mx-3 mb-6">
+			<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+		  		<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+          		      Title
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Title" name="title" value="{{ $a->title }}" required>
+          		  </div>
+          		</div>
+				<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+					  Category
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Category" name="category" value="General" required>
+          		  </div>
+          		</div>
+	      		<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+					  Description
+          		    </label>
+          		    <textarea class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" rows="4" name="description" required>{{ $a->description }}</textarea>
+          		  </div>
+          		</div>
+		  		<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+          		      Picture
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="file" name="picture" required>
+          		  </div>
+          		</div>
+				<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+					  Source
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Source" name="source"  value="{{ $a->source }}" required>
+          		  </div>
+          		</div>
+				<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+					  Author
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Author" name="author" value="{{ $a->author }}" value="Admin" required>
+          		  </div>
+          		</div>
+
+		  		<div class="flex items-center justify-between">
+          		  <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Save">
+          		    Save
+          		  </button>
+          		</div>
+			</div>
+		</div>
+
+		</form>
+  @endforeach 
+	</div>
 
 </body>
 </html>
--- resources/views/article/form.blade.php
+++ resources/views/article/form.blade.php
@@ -2,25 +2,82 @@
 <html>
 <head>
 	<title>Admin | Add Article</title>
+
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
+	<div class="container mx-auto py-24 px-12">
+    	<form class="w-full" action="/admin/article/store" method="post" enctype="multipart/form-data">
 
-	<h3>Article Data</h3>
+		<div class="flex items-center justify-between mb-12">
+    	    <div class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+    	      <a href="/admin/article"> Back</a>
+    	    </div>
+    	</div>
+		
+		<p class="text-3xl mb-6">Add Article Data</p>
 
-	<a href="/admin/article"> Back</a>
-	
-	<br/>
-	<br/>
+		<div class="flex flex-wrap -mx-3 mb-6">
+			<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+		  		<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+          		      Title
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Title" name="title" required>
+          		  </div>
+          		</div>
+				<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+					  Category
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Category" name="category" value="General" required>
+          		  </div>
+          		</div>
+	      		<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+					  Description
+          		    </label>
+          		    <textarea class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" rows="4" name="description" required></textarea>
+          		  </div>
+          		</div>
+		  		<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+          		      Picture
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="file" name="picture" required>
+          		  </div>
+          		</div>
+				<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+					  Source
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Source" name="source" required>
+          		  </div>
+          		</div>
+				<div class="flex flex-wrap -mx-3 mb-6">
+          		  <div class="w-full px-3">
+          		    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+					  Author
+          		    </label>
+          		    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Author" name="author" value="Admin" required>
+          		  </div>
+          		</div>
 
-	<form action="/admin/article/store" method="post" enctype="multipart/form-data">
-		Title <input type="text" name="title" required="required"> <br/>
-    Category <input type="text" name="category" required="required" value="General"> <br/>
-		Description <textarea name="description" required="required"></textarea> <br/>
-    Picture <input type="file" name="picture" required="required"> <br/>
-    Source <input type="text" name="source" required="required"> <br/>
-    Author <input type="text" name="author" required="required" value="Admin"> <br/>    
-    <input type="submit" value="Save"> 
-  </form>
+		  		<div class="flex items-center justify-between">
+          		  <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Save">
+          		    Save
+          		  </button>
+          		</div>
+			</div>
+		</div>
+
+		</form>
+	</div>
 
 </body>
 </html>
