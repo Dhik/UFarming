eb3cfd1be39ab7001752328303c4834892b8d0f1
Asep Budiyana M <unkn0wn@localhost.localdomain>
2020-12-02 07:59:21 +0700

    add is_done

--- app/Http/Controllers/MyPlantController.php
+++ app/Http/Controllers/MyPlantController.php
@@ -39,7 +39,7 @@ class MyPlantController extends Controller
 
   public function list() {
       $user = Auth::user();
-      $data = MyPlant::select('my_plant.id', 'plant.id AS id_plant', 'name', 'plant_name', 'progress', 'picture')
+      $data = MyPlant::select('my_plant.id', 'plant.id AS id_plant', 'name', 'plant_name', 'progress', 'picture', 'is_done')
       ->where('id_user',$user->id)
       ->join('plant', 'plant.id', '=', 'my_plant.id_plant')
       ->get();
